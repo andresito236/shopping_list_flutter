6aaa90e825a6614860a1f5bd9e8b81ec74cf4e93
andresito236 <aeparedes236@gmail.com>
2024-01-03 21:40:29 -0600

    Added dismissible and fallback content

--- lib/screens/groceries.dart
+++ lib/screens/groceries.dart
@@ -11,13 +11,11 @@ class GroceriesScreen extends StatefulWidget {
 }
 
 class _GroceriesScreenState extends State<GroceriesScreen> {
-
   final List<GroceryItem> _groceryItems = [];
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
-      MaterialPageRoute(builder: (ctx) => const NewItem())
-    );
+        MaterialPageRoute(builder: (ctx) => const NewItem()));
 
     if (newItem == null) {
       return;
@@ -31,27 +29,36 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Groceries'),
-        actions: [
-          IconButton(
-            onPressed: _addItem,
-            icon: const Icon(Icons.add),
-          )
-        ]
-      ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: ((context, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          trailing: Text(_groceryItems[index].quantity.toString()),
-          leading: Container(
-            height: 30,
-            width: 30,
-            color: _groceryItems[index].category.color,
-          ),
-        )),
-      ),
+      appBar: AppBar(title: const Text('Your Groceries'), actions: [
+        IconButton(
+          onPressed: _addItem,
+          icon: const Icon(Icons.add),
+        )
+      ]),
+      body: _groceryItems.isEmpty
+          ? const Center(
+              child: Text('No items added yet.', style: TextStyle(fontSize: 20),),
+            )
+          : ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: ((context, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  setState(() {
+                    _groceryItems.removeAt(index);
+                  });
+                },
+                child: ListTile(
+                      title: Text(_groceryItems[index].name),
+                      trailing: Text(_groceryItems[index].quantity.toString()),
+                      leading: Container(
+                        height: 30,
+                        width: 30,
+                        color: _groceryItems[index].category.color,
+                      ),
+                    ),
+              )),
+            ),
     );
   }
 }
